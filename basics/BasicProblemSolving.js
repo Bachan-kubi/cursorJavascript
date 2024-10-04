d2553784f21743aebaa144ebeb776fb18c12f99b
Bachan Kubi <felixghagra@gmail.com>
2024-10-04 05:27:33 -0700

    exam marks added

--- basics/BasicProblemSolving.js
+++ basics/BasicProblemSolving.js
@@ -232,4 +232,13 @@ function newString(a){
   }
 }
 console.log(newString('aaaAAA'));
+//38. Write a JavaScript program to evaluate a student's total marks across various examinations and determine their grade. The grading criteria are as follows: If the total marks fall within the range of 89 to 100 (inclusive), the student receives an A+ grade. If the examination is labeled as "Final-exam," the student will receive an A+ grade only if their total marks are 90 or greater. Assume that final examination means we pass 'true' as second parameter otherwise blank. If the student achieves an A+ grade, the program should return 'true'; otherwise, it should return 'false'.
 
+function mark(totalMark, isFinalExam){
+  if(isFinalExam){
+    return totalMark>=90?true: false
+  }else{
+    return totalMark>=89 && totalMark<=100?true:false
+  }
+};
+console.log(mark(98, false));
